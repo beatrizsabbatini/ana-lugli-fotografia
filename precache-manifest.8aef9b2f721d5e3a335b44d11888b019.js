180fcb86a910d53487f207faac73be24b0592a0f
Beatriz Schwartz Sabbatini <43048941+beatrizsabbatini@users.noreply.github.com>
2020-11-03 20:19:18 -0300

    Updates

---
+++ precache-manifest.8aef9b2f721d5e3a335b44d11888b019.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba726563739b0abbb7f32d20861e0ee6",
+    "url": "/ana-lugli-fotografia/index.html"
+  },
+  {
+    "revision": "a8efdccf9d07f3273ca2",
+    "url": "/ana-lugli-fotografia/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5709e81944aaf746cec0",
+    "url": "/ana-lugli-fotografia/static/css/main.5aef1ac2.chunk.css"
+  },
+  {
+    "revision": "a8efdccf9d07f3273ca2",
+    "url": "/ana-lugli-fotografia/static/js/2.04ea30a4.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/ana-lugli-fotografia/static/js/2.04ea30a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5709e81944aaf746cec0",
+    "url": "/ana-lugli-fotografia/static/js/main.3c8957cb.chunk.js"
+  },
+  {
+    "revision": "85fd7829f17d6a6bcad9",
+    "url": "/ana-lugli-fotografia/static/js/runtime-main.4362b8b9.js"
+  },
+  {
+    "revision": "b4a9988e11b1f113bde6639f28073baf",
+    "url": "/ana-lugli-fotografia/static/media/ana.b4a9988e.jpg"
+  },
+  {
+    "revision": "f5fe238666c14d132329754a1eaa08c1",
+    "url": "/ana-lugli-fotografia/static/media/decoration-line.f5fe2386.svg"
+  },
+  {
+    "revision": "2ba8005ba7264af87a0e71a5424ff4e9",
+    "url": "/ana-lugli-fotografia/static/media/instagram.2ba8005b.svg"
+  },
+  {
+    "revision": "9f47f5a5dba48dc3ba1674f8497639ed",
+    "url": "/ana-lugli-fotografia/static/media/logo.9f47f5a5.svg"
+  }
+]);
